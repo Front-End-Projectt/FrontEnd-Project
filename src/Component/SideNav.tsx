cba280330765c60aec5adc2295d4f1e977a6095e
Abdalazez <abalanazi@nadeer.com.sa>
2023-02-09 23:28:39 +0300

    fix SideNav

--- src/Component/SideNav.tsx
+++ src/Component/SideNav.tsx
@@ -89,8 +89,8 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     <Box
       transition="3s ease"
       bg={useColorModeValue('white', 'gray.900')}
-      borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      borderLeft="1px"
+      borderLeftColor={useColorModeValue('gray.200', 'gray.700')}
       w={{ base: 'full', md: 60 }}
       pos="fixed"
       h="full"
