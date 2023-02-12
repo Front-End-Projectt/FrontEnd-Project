d07104a591f8cb4043681a7ca325684826691b7c
Rayan Jowair <rayanaljowair@gmail.com>
2023-02-12 11:44:51 +0300

    add Hospital.tsx, update app.tsx 'Routes'

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import SideNav from './Component/SideNav'
-import { Route, Routes } from 'react-router-dom'
-import Diagnosis from './Component/Diagnosis'
-import Register from './Component/Authentication/Register'
-import SignIn from './Component/Authentication/SignIn'
-import Buttons from './Component/Authentication/Buttons'
-import './App.css'
+import SideNav from "./Component/SideNav";
+import { Route, Routes } from "react-router-dom";
+import Diagnosis from "./Component/Diagnosis";
+import Register from "./Component/Authentication/Register";
+import SignIn from "./Component/Authentication/SignIn";
+import Buttons from "./Component/Authentication/Buttons";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      
-      <Route path='SideNav' element={<SideNav comp={<Buttons/>} children={undefined}/>}></Route>
-      <Route path='diagnosis' element={<Diagnosis/>}></Route>
-      <Route path='register' element={<Register/>}></Route>
-      <Route path='signIn' element={<SignIn/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<SignIn />}></Route>
+        <Route
+          path="SideNav"
+          element={<SideNav comp={<Buttons />} children={undefined} />}></Route>
+        <Route path="diagnosis" element={<Diagnosis />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="signIn" element={<SignIn />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
