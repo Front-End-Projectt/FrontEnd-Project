247b7a54d5cb2e40ba6f632aa8541c5e603fdbea
av4a <a.k.a.502.2000@gmail.com>
2023-02-12 23:12:32 +0300

    I pass a value from Cards:Child to PECS:Parent.

--- src/Component/PECS/PECS.tsx
+++ src/Component/PECS/PECS.tsx
@@ -1,19 +1,39 @@
-import { Box, Flex, Icon, IconButton, SimpleGrid } from '@chakra-ui/react'
-import React, { ReactNode } from 'react'
+import {Image, Box, Card, CardBody, Flex, Heading, Icon, IconButton, SimpleGrid } from '@chakra-ui/react'
+import React, { ReactNode, useState } from 'react'
 import { StarIcon, DeleteIcon } from '@chakra-ui/icons'
 import categories from './Categories'
 import Categories from './Categories'
 import { Route, Routes } from 'react-router-dom'
 import Cards from './Cards'
 
-
 function PECS( ) {
-  
+  const [info, setInfo] = useState<any>({})
+console.log();
+
+function checkInfo() {
+  if(!Object.keys(info).length){
+    return <></>
+  }else{
+    return [info].map( e => 
+      <Card w={100} key={e.title}  maxW='sm' textAlign={"center"} borderRadius={30}  cursor={"pointer"} >
+          <CardBody>
+              <Image
+              src= {e.img}
+              alt={`${e.title}`}
+              />
+              <Heading mt='6' size='md'>{e.title}</Heading>
+          </CardBody>
+      </Card>
+      )
+  }
+}
+
 
   return (
     <>
         <Flex p={4} justifyContent={"space-between"} border={"1px solid"}>
-            <Flex p={4}>
+            <Flex p={4} >
+              { checkInfo() }
             </Flex>
             <Flex alignItems={"end"}>
               <IconButton  size="lg" variant="ghost" icon={<StarIcon />} aria-label={''}/>
@@ -24,7 +44,7 @@ function PECS( ) {
 
           <Routes>
             <Route path='/Categories' element={<Categories/>}/>
-            <Route path='/Cards' element={<Cards/>}/>
+            <Route path='/Cards' element={<Cards setInfo={setInfo}/>}/>
           </Routes>
 
         </SimpleGrid>
