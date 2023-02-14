985903e9fa6b8ba7abae103b2a856f63f99a7dba
Rayan Jowair <rayanaljowair@gmail.com>
2023-02-14 11:52:48 +0300

    update cards heading in hospitals.tsx

--- src/Component/Hospitals/Hospitals.tsx
+++ src/Component/Hospitals/Hospitals.tsx
@@ -10,11 +10,11 @@ import {
   Stack,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import HospitalsDetails from './HospitalsDetails';
-import hospArr from './hospitalArray.js';
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import HospitalsDetails from "./HospitalsDetails";
+import hospArr from "./hospitalArray.js";
 
 function Hospitals() {
   const navigate = useNavigate();
@@ -22,12 +22,12 @@ function Hospitals() {
   const [arr, setArr] = useState(hospArr);
 
   const filter = (city: string) => {
-    if (city === 'الرياض') {
-      setArr(hospArr.filter((e) => e.city === 'الرياض'));
-    } else if (city === 'الاحساء') {
-      setArr(hospArr.filter((e) => e.city === 'الاحساء'));
-    } else if (city === 'حائل') {
-      setArr(hospArr.filter((e) => e.city === 'حائل'));
+    if (city === "الرياض") {
+      setArr(hospArr.filter((e) => e.city === "الرياض"));
+    } else if (city === "الاحساء") {
+      setArr(hospArr.filter((e) => e.city === "الاحساء"));
+    } else if (city === "حائل") {
+      setArr(hospArr.filter((e) => e.city === "حائل"));
     } else {
       setArr(hospArr);
     }
@@ -35,8 +35,10 @@ function Hospitals() {
 
   return (
     <Box m={5}>
-      <Heading mb={2}>المستشفيات</Heading>
-      <Text fontSize={'lg'}>
+      <Heading mb={2} fontSize={"3xl"}>
+        المستشفيات
+      </Heading>
+      <Text fontSize={"lg"}>
         المستشفي هو وسيلة للرعاية الصحية الذاتية التي توفر خدمات الرعاية الصحية
         الأساسية والآثار التتبعية للحالات الصحية على المستويات المختلفة. ويقدم
         المستشفي خدمات طبية، علاجية، صحية راجعة وأخرى كخدمات مرافقة (استقبال
@@ -46,15 +48,14 @@ function Hospitals() {
         غيرها من الخدمات.
       </Text>
       <Select
-        bg={'white'}
+        bg={"white"}
         pr={3}
-        _hover={{ cursor: 'pointer' }}
+        _hover={{ cursor: "pointer" }}
         placeholder="المدينة"
         w={200}
-        size={'md'}
+        size={"md"}
         m={5}
-        onChange={(e) => filter(e.target.value)}
-      >
+        onChange={(e) => filter(e.target.value)}>
         <option value="الرياض">الرياض</option>
         <option value="الاحساء">الاحساء</option>
         <option value="حائل">حائل</option>
@@ -62,57 +63,54 @@ function Hospitals() {
       <Box>
         <SimpleGrid
           columns={{ base: 1, lg: 2, xl: 3 }}
-          justifyContent={'center'}
-          spacing={10}
-        >
+          justifyContent={"center"}
+          spacing={10}>
           {arr.map((value) => (
             <Box
               key={value.title}
-              display={'flex'}
-              justifyContent={'space-between'}
-              flexDirection={'column'}
-              maxW={'445px'}
-              w={'full'}
-              bg={useColorModeValue('white', 'gray.900')}
-              boxShadow={'2xl'}
-              rounded={'md'}
+              display={"flex"}
+              justifyContent={"space-between"}
+              flexDirection={"column"}
+              maxW={"445px"}
+              w={"full"}
+              bg={useColorModeValue("white", "gray.900")}
+              boxShadow={"lg"}
+              rounded={"md"}
               p={6}
-              overflow={'hidden'}
-            >
-              <Heading fontSize={'3xl'} mb={5}>{value.title}</Heading>
+              overflow={"hidden"}>
+              <Heading fontSize={{ base: "3xl", md: "xl" }} mb={5}>
+                {value.title}
+              </Heading>
               <Stack>
                 <Text
-                  overflow={'hidden'}
-                  whiteSpace={'nowrap'}
-                  textOverflow={'ellipsis'}
-                  color={'gray.500'}
-                >
+                  overflow={"hidden"}
+                  whiteSpace={"nowrap"}
+                  textOverflow={"ellipsis"}
+                  color={"gray.500"}>
                   {value.description}
                 </Text>
               </Stack>
               <Flex
-                flexDirection={'row'}
-                justifyContent={'space-between'}
-                alignItems={'flex-end'}
-              >
+                flexDirection={"row"}
+                justifyContent={"space-between"}
+                alignItems={"flex-end"}>
                 <Badge
                   px={2}
                   py={1}
-                  bg={useColorModeValue('gray.50', 'gray.800')}
-                  fontWeight={'400'}
-                >
+                  bg={useColorModeValue("gray.50", "gray.800")}
+                  fontWeight={"400"}>
                   #{value.city}
                 </Badge>
                 <Button
                   _hover={{
-                    backgroundColor: 'rgba(0, 135, 85, 0.7)',
-                    color: '#ffffff',
-                    transition: '150ms',
+                    backgroundColor: "rgba(0, 135, 85, 0.7)",
+                    color: "#ffffff",
+                    transition: "150ms",
                   }}
                   variant="outline"
                   mt={5}
-                  onClick={() => navigate(`/Hospitals/${value.id}`)}
-                >
+                  fontSize="sm"
+                  onClick={() => navigate(`/Hospitals/${value.id}`)}>
                   عرض المزيد
                 </Button>
               </Flex>
